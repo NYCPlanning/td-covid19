41c90dd0e7e8580c60e9064e10352b26910e9e14
mayijun1203 <mayijun1203@gmail.com>
2020-03-30 16:00:37 -0400

    Update turnstile.py

--- subway/turnstile.py
+++ subway/turnstile.py
@@ -77,7 +77,7 @@ def unitcascpexit(ucsexit):
 
 # Compile data
 tp=pd.DataFrame()
-for i in sorted(os.listdir(path+'DATA'))[0:2]:
+for i in sorted(os.listdir(path+'DATA'))[-5:-1]:
     tp=pd.concat([tp,pd.read_csv(path+'DATA/'+str(i),dtype=str)],ignore_index=True)
 tp['id']=tp['UNIT']+'|'+tp['C/A']+'|'+tp['SCP']
 tp['unit']=tp['UNIT'].copy()
@@ -87,72 +87,72 @@ tp['firstdesc']=tp['DESC'].copy()
 tp['firstentries']=pd.to_numeric(tp['ENTRIES'])
 tp['firstexits']=pd.to_numeric(tp['EXITS                                                               '])
 tp=tp[['id','unit','firstdate','firsttime','firstdesc','firstentries','firstexits']].reset_index(drop=True)
-#
-## Entries
-#df=pd.DataFrame()
-#for i in list(rc['Remote'].unique()):
-#    try:
-#        tpunit=tp[tp['unit']==i].reset_index(drop=True)
-#        rtunit=rt[rt['Remote']==i].reset_index(drop=True)
-#        rttp=datetime.datetime.strptime(tpunit['firstdate'].unique()[0]+' '+rtunit.loc[0,'Time'],'%m/%d/%Y %H:%M:%S').astimezone(pytz.timezone('America/New_York')).timestamp()
-#        rtlist=[rttp]
-#        for j in range(0,len(tpunit['firstdate'].unique())*6-1):
-#            rttp+=4*3600
-#            rtlist+=[rttp]
-#        rtlist=[datetime.datetime.fromtimestamp(x).astimezone(pytz.timezone('America/New_York')) for x in rtlist]
-#        rtunit=pd.concat([rtunit]*len(rtlist),ignore_index=True)
-#        rtunit['unit']=rtunit['Remote'].copy()
-#        rtunit['firstdate']=[x.strftime('%m/%d/%Y') for x in rtlist]
-#        rtunit['firsttime']=[x.strftime('%H:%M:%S') for x in rtlist]
-#        rtunit=rtunit[['unit','firstdate','firsttime']].reset_index(drop=True)
-#        tpucs=tpunit.groupby('id',as_index=False).apply(unitcascpentry).reset_index(drop=True)
-#        df=pd.concat([df,tpucs],axis=0,ignore_index=True)
-#    except:
-#        print(str(i))
-#df=df[['id','unit','firstdate','time','entries','flagtime','flagentry']].reset_index(drop=True)
-#dfunitentry=df.groupby(['unit','firstdate','time'],as_index=False).agg({'entries':'sum','flagtime':'sum','flagentry':'sum','id':'count'}).reset_index(drop=True)
-#dfunitentry.to_csv(path+'dfunitentry.csv',index=False)
-#dfdateentry=dfunitentry[(dfunitentry['flagtime']==0)&(dfunitentry['flagentry']==0)].reset_index(drop=True)
-#dfdateentry=dfdateentry.groupby('firstdate',as_index=False).agg({'entries':'sum'}).reset_index(drop=True)
-#dfflagtimeentry=dfunitentry.groupby('firstdate',as_index=False).agg({'flagtime':'sum'}).reset_index(drop=True)
-#dfflagentry=dfunitentry.groupby('firstdate',as_index=False).agg({'flagentry':'sum'}).reset_index(drop=True)
-#dfdateentry=pd.merge(dfdateentry,dfflagtimeentry,how='left',on='firstdate')
-#dfdateentry=pd.merge(dfdateentry,dfflagentry,how='left',on='firstdate')
-#dfdateentry['firstdate']=[datetime.datetime.strptime(x,'%m/%d/%Y') for x in dfdateentry['firstdate']]
-#dfdateentry=dfdateentry.sort_values('firstdate').reset_index(drop=True)
-#dfdateentry.to_csv(path+'dfdateentry.csv',index=False)
-#
-## Exits
-#df=pd.DataFrame()
-#for i in list(rc['Remote'].unique()):
-#    try:
-#        tpunit=tp[tp['unit']==i].reset_index(drop=True)
-#        rtunit=rt[rt['Remote']==i].reset_index(drop=True)
-#        rttp=datetime.datetime.strptime(tpunit['firstdate'].unique()[0]+' '+rtunit.loc[0,'Time'],'%m/%d/%Y %H:%M:%S').astimezone(pytz.timezone('America/New_York')).timestamp()
-#        rtlist=[rttp]
-#        for j in range(0,len(tpunit['firstdate'].unique())*6-1):
-#            rttp+=4*3600
-#            rtlist+=[rttp]
-#        rtlist=[datetime.datetime.fromtimestamp(x).astimezone(pytz.timezone('America/New_York')) for x in rtlist]
-#        rtunit=pd.concat([rtunit]*len(rtlist),ignore_index=True)
-#        rtunit['unit']=rtunit['Remote'].copy()
-#        rtunit['firstdate']=[x.strftime('%m/%d/%Y') for x in rtlist]
-#        rtunit['firsttime']=[x.strftime('%H:%M:%S') for x in rtlist]
-#        rtunit=rtunit[['unit','firstdate','firsttime']].reset_index(drop=True)
-#        tpucs=tpunit.groupby('id',as_index=False).apply(unitcascpexit).reset_index(drop=True)
-#        df=pd.concat([df,tpucs],axis=0,ignore_index=True)
-#    except:
-#        print(str(i))
-#df=df[['id','unit','firstdate','time','exits','flagtime','flagexit']].reset_index(drop=True)
-#dfunitexit=df.groupby(['unit','firstdate','time'],as_index=False).agg({'exits':'sum','flagtime':'sum','flagexit':'sum','id':'count'}).reset_index(drop=True)
-#dfunitexit.to_csv(path+'dfunitexit.csv',index=False)
-#dfdateexit=dfunitexit[(dfunitexit['flagtime']==0)&(dfunitexit['flagexit']==0)].reset_index(drop=True)
-#dfdateexit=dfdateexit.groupby('firstdate',as_index=False).agg({'exits':'sum'}).reset_index(drop=True)
-#dfflagtimeexit=dfunitexit.groupby('firstdate',as_index=False).agg({'flagtime':'sum'}).reset_index(drop=True)
-#dfflagexit=dfunitexit.groupby('firstdate',as_index=False).agg({'flagexit':'sum'}).reset_index(drop=True)
-#dfdateexit=pd.merge(dfdateexit,dfflagtimeexit,how='left',on='firstdate')
-#dfdateexit=pd.merge(dfdateexit,dfflagexit,how='left',on='firstdate')
-#dfdateexit['firstdate']=[datetime.datetime.strptime(x,'%m/%d/%Y') for x in dfdateexit['firstdate']]
-#dfdateexit=dfdateexit.sort_values('firstdate').reset_index(drop=True)
-#dfdateexit.to_csv(path+'dfdateexit.csv',index=False)
+
+# Entries
+df=pd.DataFrame()
+for i in list(rc['Remote'].unique()):
+    try:
+        tpunit=tp[tp['unit']==i].reset_index(drop=True)
+        rtunit=rt[rt['Remote']==i].reset_index(drop=True)
+        rttp=datetime.datetime.strptime(tpunit['firstdate'].unique()[0]+' '+rtunit.loc[0,'Time'],'%m/%d/%Y %H:%M:%S').astimezone(pytz.timezone('America/New_York')).timestamp()
+        rtlist=[rttp]
+        for j in range(0,len(tpunit['firstdate'].unique())*6-1):
+            rttp+=4*3600
+            rtlist+=[rttp]
+        rtlist=[datetime.datetime.fromtimestamp(x).astimezone(pytz.timezone('America/New_York')) for x in rtlist]
+        rtunit=pd.concat([rtunit]*len(rtlist),ignore_index=True)
+        rtunit['unit']=rtunit['Remote'].copy()
+        rtunit['firstdate']=[x.strftime('%m/%d/%Y') for x in rtlist]
+        rtunit['firsttime']=[x.strftime('%H:%M:%S') for x in rtlist]
+        rtunit=rtunit[['unit','firstdate','firsttime']].reset_index(drop=True)
+        tpucs=tpunit.groupby('id',as_index=False).apply(unitcascpentry).reset_index(drop=True)
+        df=pd.concat([df,tpucs],axis=0,ignore_index=True)
+    except:
+        print(str(i))
+df=df[['id','unit','firstdate','time','entries','flagtime','flagentry']].reset_index(drop=True)
+dfunitentry=df.groupby(['unit','firstdate','time'],as_index=False).agg({'entries':'sum','flagtime':'sum','flagentry':'sum','id':'count'}).reset_index(drop=True)
+dfunitentry.to_csv(path+'dfunitentry.csv',index=False)
+dfdateentry=dfunitentry[(dfunitentry['flagtime']==0)&(dfunitentry['flagentry']==0)].reset_index(drop=True)
+dfdateentry=dfdateentry.groupby('firstdate',as_index=False).agg({'entries':'sum'}).reset_index(drop=True)
+dfflagtimeentry=dfunitentry.groupby('firstdate',as_index=False).agg({'flagtime':'sum'}).reset_index(drop=True)
+dfflagentry=dfunitentry.groupby('firstdate',as_index=False).agg({'flagentry':'sum'}).reset_index(drop=True)
+dfdateentry=pd.merge(dfdateentry,dfflagtimeentry,how='left',on='firstdate')
+dfdateentry=pd.merge(dfdateentry,dfflagentry,how='left',on='firstdate')
+dfdateentry['firstdate']=[datetime.datetime.strptime(x,'%m/%d/%Y') for x in dfdateentry['firstdate']]
+dfdateentry=dfdateentry.sort_values('firstdate').reset_index(drop=True)
+dfdateentry.to_csv(path+'dfdateentry.csv',index=False)
+
+# Exits
+df=pd.DataFrame()
+for i in list(rc['Remote'].unique()):
+    try:
+        tpunit=tp[tp['unit']==i].reset_index(drop=True)
+        rtunit=rt[rt['Remote']==i].reset_index(drop=True)
+        rttp=datetime.datetime.strptime(tpunit['firstdate'].unique()[0]+' '+rtunit.loc[0,'Time'],'%m/%d/%Y %H:%M:%S').astimezone(pytz.timezone('America/New_York')).timestamp()
+        rtlist=[rttp]
+        for j in range(0,len(tpunit['firstdate'].unique())*6-1):
+            rttp+=4*3600
+            rtlist+=[rttp]
+        rtlist=[datetime.datetime.fromtimestamp(x).astimezone(pytz.timezone('America/New_York')) for x in rtlist]
+        rtunit=pd.concat([rtunit]*len(rtlist),ignore_index=True)
+        rtunit['unit']=rtunit['Remote'].copy()
+        rtunit['firstdate']=[x.strftime('%m/%d/%Y') for x in rtlist]
+        rtunit['firsttime']=[x.strftime('%H:%M:%S') for x in rtlist]
+        rtunit=rtunit[['unit','firstdate','firsttime']].reset_index(drop=True)
+        tpucs=tpunit.groupby('id',as_index=False).apply(unitcascpexit).reset_index(drop=True)
+        df=pd.concat([df,tpucs],axis=0,ignore_index=True)
+    except:
+        print(str(i))
+df=df[['id','unit','firstdate','time','exits','flagtime','flagexit']].reset_index(drop=True)
+dfunitexit=df.groupby(['unit','firstdate','time'],as_index=False).agg({'exits':'sum','flagtime':'sum','flagexit':'sum','id':'count'}).reset_index(drop=True)
+dfunitexit.to_csv(path+'dfunitexit.csv',index=False)
+dfdateexit=dfunitexit[(dfunitexit['flagtime']==0)&(dfunitexit['flagexit']==0)].reset_index(drop=True)
+dfdateexit=dfdateexit.groupby('firstdate',as_index=False).agg({'exits':'sum'}).reset_index(drop=True)
+dfflagtimeexit=dfunitexit.groupby('firstdate',as_index=False).agg({'flagtime':'sum'}).reset_index(drop=True)
+dfflagexit=dfunitexit.groupby('firstdate',as_index=False).agg({'flagexit':'sum'}).reset_index(drop=True)
+dfdateexit=pd.merge(dfdateexit,dfflagtimeexit,how='left',on='firstdate')
+dfdateexit=pd.merge(dfdateexit,dfflagexit,how='left',on='firstdate')
+dfdateexit['firstdate']=[datetime.datetime.strptime(x,'%m/%d/%Y') for x in dfdateexit['firstdate']]
+dfdateexit=dfdateexit.sort_values('firstdate').reset_index(drop=True)
+dfdateexit.to_csv(path+'dfdateexit.csv',index=False)
 
