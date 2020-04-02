d42218388d1f098cbb19f51e0d78e8ed6feb1555
mayijun1203 <mayijun1203@gmail.com>
2020-04-02 14:57:05 -0400

    Update service.py

--- subway/service.py
+++ subway/service.py
@@ -11,10 +11,9 @@ path='C:/Users/Yijun Ma/Desktop/D/DOCUMENT/DCP2020/COVID19/SUBWAY/SERVICE/'
 calendar=pd.read_csv(path+'google_transit/calendar.txt')
 trips=pd.read_csv(path+'google_transit/trips.txt')
 stoptimes=pd.read_csv(path+'google_transit/stop_times.txt')
+stoptimes['hour']=[str(x)[0:2] for x in stoptimes['arrival_time']]
 tripfirsttime=stoptimes.drop_duplicates('trip_id',keep='first').reset_index(drop=True)
-tripfirsttime['hour']=[str(x)[0:2] for x in tripfirsttime['arrival_time']]
 triplasttime=stoptimes.drop_duplicates('trip_id',keep='last').reset_index(drop=True)
-triplasttime['hour']=[str(x)[0:2] for x in triplasttime['arrival_time']]
 
 
 
@@ -40,3 +39,15 @@ for i in ['monday','tuesday','wednesday','thursday','friday','saturday','sunday'
     df=pd.concat([df,tp],axis=0,ignore_index=True)
 df.to_csv(path+'df.csv',index=False)
 
+
+
+
+df=pd.DataFrame()
+for i in ['monday','tuesday','wednesday','thursday','friday','saturday','sunday']:
+    serviceid=list(calendar.loc[calendar[i]==1,'service_id'])
+    tripid=list(trips.loc[np.isin(trips['service_id'],serviceid),'trip_id'])
+    tp=stoptimes[np.isin(stoptimes['trip_id'],tripid)].reset_index(drop=True)
+    tp=tp.groupby('hour',as_index=False).agg({'trip_id':'count'}).reset_index(drop=True)
+    tp['day']=str(i)
+    df=pd.concat([df,tp],axis=0,ignore_index=True)
+df.to_csv(path+'df.csv',index=False)
