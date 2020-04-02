bbb661b5a222d3e812467ccc771a69649e8043a6
mayijun1203 <mayijun1203@gmail.com>
2020-04-02 13:03:20 -0400

    Update service.py

--- subway/service.py
+++ subway/service.py
@@ -1,3 +1,38 @@
 import pandas as pd
+import numpy as np
+import datetime
+import pytz
 
-stoptime=pd.read_csv('C:/Users/Yijun Ma/Desktop/google_transit/stop_times.txt')
+
+
+pd.set_option('display.max_columns', None)
+path='C:/Users/Yijun Ma/Desktop/D/DOCUMENT/DCP2020/COVID19/SUBWAY/SERVICE/'
+
+
+
+calendar=pd.read_csv(path+'google_transit/calendar.txt')
+trips=pd.read_csv(path+'google_transit/trips.txt')
+stoptimes=pd.read_csv(path+'google_transit/stop_times.txt')
+
+for i in ['monday','tuesday','wednesday','thursday','friday','saturday','sunday']:
+    serviceid=list(calendar.loc[calendar[i]==1,'service_id'])
+    tripid=list(trips.loc[np.isin(trips['service_id'],serviceid),'trip_id'])
+    print(str(i)+': '+str(len(tripid)))
+
+i='wednesday'
+k=stoptimes.drop_duplicates('trip_id',keep='first')
+k=k[np.isin(k['trip_id'],tripid)]
+k['hour']=[str(x)[0:2] for x in k['arrival_time']]
+k=k.groupby('hour',as_index=False).agg({'trip_id':'count'})
+print(k)
+
+i='thursday'
+k=stoptimes.drop_duplicates('trip_id',keep='first')
+k=k[np.isin(k['trip_id'],tripid)]
+k['hour']=[str(x)[0:2] for x in k['arrival_time']]
+k=k.groupby('hour',as_index=False).agg({'trip_id':'count'})
+print(k)
+
+
+k=stoptimes[stoptimes['trip_id']=='BFA19GEN-A083-Weekday-00_156200_A..N09R']
+k=trips[trips['trip_id']=='BFA19GEN-A083-Weekday-00_156200_A..N09R']
