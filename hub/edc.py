67088684ec23f2ae1e79e7ebaae167d8b625ba11
mayijun1203 <mayijun1203@gmail.com>
2020-05-10 14:21:32 -0400

    Create edc.py

---
+++ hub/edc.py
@@ -0,0 +1,14 @@
+import urllib.request
+import shutil
+import os
+import pandas as pd
+import numpy as np
+import datetime
+import pytz
+
+
+
+pd.set_option('display.max_columns', None)
+path='C:/Users/Yijun Ma/Desktop/D/DOCUMENT/DCP2020/COVID19/HUB/EDC/'
+
+
