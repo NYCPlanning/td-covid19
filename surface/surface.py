4ccea20afc9ec2692b80bd38cf856ba91a7aa310
mayijun1203 <mayijun1203@gmail.com>
2020-05-13 11:15:27 -0400

    Update surface.py

--- surface/surface.py
+++ surface/surface.py
@@ -3,5 +3,13 @@ import pandas as pd
 pd.set_option('display.max_columns', None)
 path='C:/Users/Yijun Ma/Desktop/D/DOCUMENT/DCP2020/COVID19/SURFACE/'
 
-df=pd.read_csv(path+'psam_p36.csv',dtype=str)
+pdf=pd.read_csv(path+'psam_p36.csv',dtype=str)
+p=pdf[['SERIALNO','SPORDER','PUMA','ST','NAICSP','JWTR','RAC1P','HISP','JWMNP','PWGTP']]
+p.to_csv(path+'p.csv',index=False)
+
+
+hdf=pd.read_csv(path+'psam_h36.csv',dtype=str)
+h=hdf[['SERIALNO','HINCP']]
+h.to_csv(path+'h.csv',index=False)
+
 
