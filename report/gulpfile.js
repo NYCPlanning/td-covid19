9097928f03ba8cc7687d5dba3ed4fdf153b32a97
Yijun Ma <mayijun1203@gmail.com>
2020-09-13 17:22:13 -0400

    Update gulpfile.js

--- report/gulpfile.js
+++ report/gulpfile.js
@@ -10,28 +10,35 @@ var gulp = require('gulp'),
     cleanCss = require('gulp-clean-css'),
     flatmap = require('gulp-flatmap'),
     htmlmin = require('gulp-htmlmin'),
+    zip = require('gulp-zip');
+
+gulp.task('archive', function () {
+    return gulp.src('dist/**')
+        .pipe(zip('dist.zip'))
+        .pipe(gulp.dest('archive'));
+});
 
 gulp.task('clean', function () {
     return del(['dist']);
 });
 
 gulp.task('copyplotly', function () {
-    return gulp.src('plotly/*')
+    return gulp.src('plotly/**')
         .pipe(gulp.dest('dist/plotly'));
 });
 
 gulp.task('copymapbox', function () {
-    return gulp.src('mapbox/*')
+    return gulp.src('mapbox/**')
         .pipe(gulp.dest('dist/mapbox'));
 });
 
 gulp.task('copypdf', function () {
-    return gulp.src('pdf/*')
+    return gulp.src('pdf/**')
         .pipe(gulp.dest('dist/pdf'));
 });
 
 gulp.task('imagemin', function () {
-    return gulp.src('img/*.{png,jpg,gif}')
+    return gulp.src('img/**/*.{png,jpg,gif}')
         .pipe(imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
         .pipe(gulp.dest('dist/img'));
 });
@@ -51,7 +58,7 @@ gulp.task('usemin', function () {
         .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('build', gulp.series('clean', gulp.parallel('copyplotly', 'copymapbox', 'copypdf', 'imagemin', 'usemin')), function (done) {
+gulp.task('build', gulp.series('archive', 'clean', gulp.parallel('copyplotly', 'copymapbox', 'copypdf', 'imagemin', 'usemin')), function (done) {
     done();
 });
 
