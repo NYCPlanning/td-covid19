a5e5efcd46e64fb7ecb35cf000ca7c38ab565265
Yijun Ma <mayijun1203@gmail.com>
2020-09-13 16:54:31 -0400

    update

--- report/gulpfile.js
+++ report/gulpfile.js
@@ -2,8 +2,6 @@
 
 
 var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    browserSync = require('browser-sync'),
     del = require('del'),
     imagemin = require('gulp-imagemin'),
     uglify = require('gulp-uglify'),
@@ -14,42 +12,23 @@ var gulp = require('gulp'),
     htmlmin = require('gulp-htmlmin');
 
 
-gulp.task('sass', function () {
-    return gulp.src('./css/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
-});
-
-gulp.task('sass:watch', function () {
-    gulp.watch('./css/*.scss', gulp.series('sass'));
-});
-
-gulp.task('browser-sync', function () {
-    var files = [
-        './*html',
-        './css/*.css',
-        './js/*.js',
-        './img/*.{png,jpg,gif}'
-    ];
-
-    browserSync.init(files, {
-        server: {
-            baseDir: './'
-        }
-    });
-});
-
-gulp.task('default', gulp.parallel('browser-sync', 'sass:watch'), function (done) {
-    done();
-});
-
 gulp.task('clean', function () {
     return del(['dist']);
 });
 
-gulp.task('copyfonts', function () {
-    return gulp.src('./node_modules/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
-        .pipe(gulp.dest('./dist/fonts'));
+gulp.task('copyplotly', function () {
+    return gulp.src('plotly/*')
+        .pipe(gulp.dest('dist/plotly'));
+});
+
+gulp.task('copypdf', function () {
+    return gulp.src('pdf/*')
+        .pipe(gulp.dest('dist/pdf'));
+});
+
+gulp.task('copymapbox', function () {
+    return gulp.src('mapbox/*')
+        .pipe(gulp.dest('dist/mapbox'));
 });
 
 gulp.task('imagemin', function () {
@@ -73,6 +52,6 @@ gulp.task('usemin', function () {
         .pipe(gulp.dest('dist/'));
 });
 
-gulp.task('build', gulp.series('clean', gulp.parallel('copyfonts', 'imagemin', 'usemin')), function (done) {
+gulp.task('build', gulp.series('clean', gulp.parallel('copyplotly')), function (done) {
     done();
 });
