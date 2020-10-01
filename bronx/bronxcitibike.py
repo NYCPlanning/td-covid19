a5e2c9fb0faa28d277230d7b2d95c542b224029d
Yijun Ma <mayijun1203@gmail.com>
2020-10-01 19:23:09 -0400

    update

---
+++ bronx/bronxcitibike.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import geopandas as gpd
+import json
+import shapely
+
+
+
+pd.set_option('display.max_columns', None)
+path='C:/Users/mayij/Desktop/DOC/DCP2020/COVID19/BRONX/'
+
+
+df=pd.read_csv(path+'bronxcitibike.csv')
+df['dailytrips'].describe(percentiles=np.arange(0.2,1,0.2))
+df['cat']=np.where(df['dailytrips']>25,'>25',
+          np.where(df['dailytrips']>20,'21~25',
+          np.where(df['dailytrips']>15,'16~20',
+          np.where(df['dailytrips']>10,'11~15',
+          '<=10'))))
+df=gpd.GeoDataFrame(df,geometry=[shapely.geometry.Point(x,y) for x,y in zip(df['lon'],df['lat'])],crs='epsg:4326')
+df.to_file(path+'bronxcitibike.geojson',driver='GeoJSON',index=True)
