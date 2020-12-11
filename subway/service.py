7facc581c0e230607412abe43810aba0c14c8c2a
Yijun Ma <mayijun1203@gmail.com>
2020-12-10 23:51:58 -0500

    Update service.py

--- subway/service.py
+++ subway/service.py
@@ -5,7 +5,7 @@ import pytz
 
 
 pd.set_option('display.max_columns', None)
-path='C:/Users/Yijun Ma/Desktop/D/DOCUMENT/DCP2020/COVID19/SUBWAY/SERVICE/'
+path='C:/Users/mayij/Desktop/DOC/DCP2020/COVID19/SUBWAY/'
 
 
 # GTFS Schedule
@@ -217,6 +217,63 @@ k['hour']=[x.strftime('%H') for x in k['time']]
 k=k.groupby('hour',as_index=False).agg({'tripid':'count'}).reset_index(drop=True)
 k.to_csv(path+'k.csv',index=False)
 
+k=pd.read_csv(path+'gtfsrt/tp_20200709.csv',dtype=float,converters={'routeid':str,'tripdate':str,'tripid':str,'startstopid':str,'endstopid':str})
+k=k[k['starttime']!=0].reset_index(drop=True)
+k=k[k['endtime']!=0].reset_index(drop=True)
+k['time']=[datetime.datetime.fromtimestamp(x,tz=pytz.timezone('America/New_York')) for x in k['starttime']]
+k['hour']=[x.strftime('%H') for x in k['time']]
+k=k.groupby('hour',as_index=False).agg({'tripid':'count'}).reset_index(drop=True)
+k.to_csv(path+'k.csv',index=False)
+
+k=pd.read_csv(path+'gtfsrt/tp_20200813.csv',dtype=float,converters={'routeid':str,'tripdate':str,'tripid':str,'startstopid':str,'endstopid':str})
+k=k[k['starttime']!=0].reset_index(drop=True)
+k=k[k['endtime']!=0].reset_index(drop=True)
+k['time']=[datetime.datetime.fromtimestamp(x,tz=pytz.timezone('America/New_York')) for x in k['starttime']]
+k['hour']=[x.strftime('%H') for x in k['time']]
+k=k.groupby('hour',as_index=False).agg({'tripid':'count'}).reset_index(drop=True)
+k.to_csv(path+'k.csv',index=False)
+
+k=pd.read_csv(path+'gtfsrt/tp_20200910.csv',dtype=float,converters={'routeid':str,'tripdate':str,'tripid':str,'startstopid':str,'endstopid':str})
+k=k[k['starttime']!=0].reset_index(drop=True)
+k=k[k['endtime']!=0].reset_index(drop=True)
+k['time']=[datetime.datetime.fromtimestamp(x,tz=pytz.timezone('America/New_York')) for x in k['starttime']]
+k['hour']=[x.strftime('%H') for x in k['time']]
+k=k.groupby('hour',as_index=False).agg({'tripid':'count'}).reset_index(drop=True)
+k.to_csv(path+'k.csv',index=False)
+
+k=pd.read_csv(path+'gtfsrt/tp_20201008.csv',dtype=float,converters={'routeid':str,'tripdate':str,'tripid':str,'startstopid':str,'endstopid':str})
+k=k[k['starttime']!=0].reset_index(drop=True)
+k=k[k['endtime']!=0].reset_index(drop=True)
+k['time']=[datetime.datetime.fromtimestamp(x,tz=pytz.timezone('America/New_York')) for x in k['starttime']]
+k['hour']=[x.strftime('%H') for x in k['time']]
+k=k.groupby('hour',as_index=False).agg({'tripid':'count'}).reset_index(drop=True)
+k.to_csv(path+'k.csv',index=False)
+
+k=pd.read_csv(path+'gtfsrt/tp_20201112.csv',dtype=float,converters={'routeid':str,'tripdate':str,'tripid':str,'startstopid':str,'endstopid':str})
+k=k[k['starttime']!=0].reset_index(drop=True)
+k=k[k['endtime']!=0].reset_index(drop=True)
+k['time']=[datetime.datetime.fromtimestamp(x,tz=pytz.timezone('America/New_York')) for x in k['starttime']]
+k['hour']=[x.strftime('%H') for x in k['time']]
+k=k.groupby('hour',as_index=False).agg({'tripid':'count'}).reset_index(drop=True)
+k.to_csv(path+'k.csv',index=False)
+
+k=pd.read_csv(path+'gtfsrt/tp_20201203.csv',dtype=float,converters={'routeid':str,'tripdate':str,'tripid':str,'startstopid':str,'endstopid':str})
+k=k[k['starttime']!=0].reset_index(drop=True)
+k=k[k['endtime']!=0].reset_index(drop=True)
+k['time']=[datetime.datetime.fromtimestamp(x,tz=pytz.timezone('America/New_York')) for x in k['starttime']]
+k['hour']=[x.strftime('%H') for x in k['time']]
+k=k.groupby('hour',as_index=False).agg({'tripid':'count'}).reset_index(drop=True)
+k.to_csv(path+'k.csv',index=False)
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -363,3 +420,81 @@ k['hour']=[x.strftime('%H') for x in k['time']]
 k=k[np.isin(k['hour'],['08','17'])].reset_index(drop=True)
 k=k.groupby('routeid',as_index=False).agg({'tripid':'count'}).reset_index(drop=True)
 k.to_csv(path+'k.csv',index=False)
+
+
+
+
+
+
+
+
+
+
+t=['10/10/2019','03/12/2020','04/09/2020','05/07/2020','06/11/2020','07/09/2020','08/13/2020','09/10/2020','10/08/2020']
+# Demand and Supply
+tp=pd.DataFrame()
+tp['Date']=t
+tp['Ridership']=np.nan
+tp['Trips']=np.nan
+for i in t:
+    # Demand
+    dfunitentry=pd.read_csv(path+'TURNSTILE/OUTPUT/dfunitentry.csv',dtype=str,converters={'entries':float,'gooducs':float,'flagtime':float,'flagentry':float})
+    amlist=['05:00:00-09:00:00','05:30:00-09:30:00','06:00:00-10:00:00','06:30:00-10:30:00','07:00:00-11:00:00',
+            '07:22:00-11:22:00','07:30:00-11:30:00','08:00:00-12:00:00','08:22:00-12:22:00','08:30:00-12:30:00']
+    cplxampre=dfunitentry[np.isin(dfunitentry['firstdate'],i)].reset_index(drop=True)
+    cplxampre=cplxampre[np.isin(cplxampre['time'],amlist)].reset_index(drop=True)
+    cplxampre=cplxampre.groupby(['unit','time'],as_index=False).agg({'entries':'mean'}).reset_index(drop=True)
+    rc=pd.read_csv(path+'TURNSTILE/RemoteComplex.csv',dtype=str,converters={'CplxID':float,'CplxLat':float,'CplxLong':float,'Hub':float})
+    cplxampre=pd.merge(cplxampre,rc,how='inner',left_on='unit',right_on='Remote')
+    cplxampre=cplxampre.groupby(['CplxID'],as_index=False).agg({'time':lambda x:'|'.join(sorted(x.unique())),'entries':'sum'}).reset_index(drop=True)
+    cplxampre.columns=['CplxID','Time','E'+i[6:10]+i[0:2]+i[3:5]]
+    cplxampre['E'+i[6:10]+i[0:2]+i[3:5]]=cplxampre['E'+i[6:10]+i[0:2]+i[3:5]]/4
+    # Supply
+    k=pd.read_csv(path+'SERVICE/gtfsrt/tp_'+i[6:10]+i[0:2]+i[3:5]+'.csv',dtype=float,converters={'routeid':str,'tripdate':str,'tripid':str,'startstopid':str,'endstopid':str})
+    k=k[k['starttime']!=0].reset_index(drop=True)
+    k=k[k['endtime']!=0].reset_index(drop=True)
+    k['time']=[datetime.datetime.fromtimestamp(x,tz=pytz.timezone('America/New_York')) for x in k['starttime']]
+    k['hour']=[x.strftime('%H') for x in k['time']]
+    k['stopid']=[x[:-1] for x in k['startstopid']]
+    k=k[['stopid','hour','tripid']].reset_index(drop=True)
+    st=pd.read_csv(path+'SERVICE/Stations.csv',dtype=str)
+    st['CplxID']=pd.to_numeric(st['Complex ID'])
+    st['stopid']=st['GTFS Stop ID'].copy()
+    st=st[['stopid','CplxID']].reset_index(drop=True)
+    k=pd.merge(k,st,on='stopid',how='inner')
+    k=pd.merge(k,cplxampre,on='CplxID',how='inner')
+    k['ct']=0
+    sorted(k.Time.unique())
+    k.loc[(k['Time']=='05:00:00-09:00:00')&(np.isin(k['hour'],['05','06','07','08'])),'ct']=1
+    k.loc[(k['Time']=='06:00:00-10:00:00')&(np.isin(k['hour'],['06','07','08','09'])),'ct']=1
+    k.loc[(k['Time']=='06:30:00-10:30:00')&(np.isin(k['hour'],['06','07','08','09'])),'ct']=1
+    k.loc[(k['Time']=='08:00:00-12:00:00')&(np.isin(k['hour'],['08','09','10','11'])),'ct']=1
+    k.loc[(k['Time']=='08:22:00-12:22:00')&(np.isin(k['hour'],['08','09','10','11'])),'ct']=1
+    k.loc[(k['Time']=='08:30:00-12:30:00')&(np.isin(k['hour'],['08','09','10','11'])),'ct']=1
+    k.loc[(k['Time']=='05:00:00-09:00:00|07:00:00-11:00:00')&(np.isin(k['hour'],['05','06','07','08','09','10'])),'ct']=1
+    k.loc[(k['Time']=='05:00:00-09:00:00|08:00:00-12:00:00')&(np.isin(k['hour'],['05','06','07','08','09','10','11'])),'ct']=1
+    k.loc[(k['Time']=='06:00:00-10:00:00|08:00:00-12:00:00')&(np.isin(k['hour'],['06','07','08','09','10','11'])),'ct']=1
+    k=k[k['ct']==1].reset_index(drop=True)
+    k=k.groupby(['CplxID','Time'],as_index=False).agg({'tripid':'count'}).reset_index(drop=True)
+    k['hours']=np.nan
+    k.loc[k['Time']=='05:00:00-09:00:00','hours']=4
+    k.loc[k['Time']=='06:00:00-10:00:00','hours']=4
+    k.loc[k['Time']=='06:30:00-10:30:00','hours']=4
+    k.loc[k['Time']=='08:00:00-12:00:00','hours']=4
+    k.loc[k['Time']=='08:22:00-12:22:00','hours']=4
+    k.loc[k['Time']=='08:30:00-12:30:00','hours']=4
+    k.loc[k['Time']=='05:00:00-09:00:00|07:00:00-11:00:00','hours']=6
+    k.loc[k['Time']=='05:00:00-09:00:00|08:00:00-12:00:00','hours']=7
+    k.loc[k['Time']=='06:00:00-10:00:00|08:00:00-12:00:00','hours']=6
+    k['T'+i[6:10]+i[0:2]+i[3:5]]=k['tripid']/k['hours']
+    k=k[['CplxID','T'+i[6:10]+i[0:2]+i[3:5]]].reset_index(drop=True)
+    k=pd.merge(k,cplxampre,on='CplxID',how='inner')
+    k=k[['CplxID','Time','E'+i[6:10]+i[0:2]+i[3:5],'T'+i[6:10]+i[0:2]+i[3:5]]].reset_index(drop=True)
+    # Summary
+    tp.loc[tp['Date']==i,'Ridership']=sum(k['E'+i[6:10]+i[0:2]+i[3:5]])
+    tp.loc[tp['Date']==i,'Trips']=sum(k['T'+i[6:10]+i[0:2]+i[3:5]])
+tp.to_csv(path+'SERVICE/ds.csv',index=False)
+
+
+
+
